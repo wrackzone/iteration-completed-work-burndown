c96b6537d20d7f71ca08515579d77856765ad720
Barry Mullan <mullan@gmail.com>
2013-07-17 08:16:55 -0400

    added ideal line

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ Ext.define('CustomApp', {
             itemId : 'iterationSelector',
             listeners: {
                     select: this._onIterationSelect,
+                    ready: this._onIterationSelect,
                     scope: this
             }
         });
@@ -35,7 +36,6 @@ Ext.define('CustomApp', {
         console.log("record",value);
         var iterationId = [value.data.ObjectID];
         this.gIteration = value.data;
-
         
         Ext.create('Rally.data.lookback.SnapshotStore', {
             autoLoad : true,
@@ -71,11 +71,27 @@ Ext.define('CustomApp', {
             {as: 'Accepted',  f: 'filteredSum', field : 'PlanEstimate', filterField : 'ScheduleState', filterValues : ['Accepted']}
         ];
         
+        var derivedFieldsAfterSummary = [
+            {   as: 'Ideal', 
+                f : function (row,index,summaryMetrics, seriesData) {
+                    console.log("row",           row);
+                    console.log("index",         index);
+                    console.log("summaryMetrics",summaryMetrics);
+                    console.log("seriesData",    seriesData);
+                    var max = seriesData[0].TaskTotal;
+                    var increments = seriesData.length - 1;
+                    var incAmount = max / increments;
+                    var ideal = Math.floor(100 * (max - index * incAmount)) / 100;
+                    return ideal > 0 ? ideal : 0;
+                }
+            }
+        ];
+        
         var config = {
           deriveFieldsOnInput: [],
           metrics: metrics,
           summaryMetricsConfig: [],
-          deriveFieldsAfterSummary: [],
+          deriveFieldsAfterSummary: derivedFieldsAfterSummary,
           granularity: lumenize.Time.DAY,
           tz: 'America/New_York',
           holidays: [],
@@ -90,7 +106,13 @@ Ext.define('CustomApp', {
         calculator.addSnapshots(snapShotData, startOnISOString, upToDateISOString);
 
         // create a high charts series config object, used to get the hc series data
-        var hcConfig = [{ name: "label" }, { name : "TaskTotal" }, { name : "TaskToDo"}, { name : 'Completed', type : 'column', yAxis:1}, {name : 'Accepted',type : 'column', yAxis:1}];
+        var hcConfig = [{ name: "label" }, 
+            { name : "TaskTotal" }, 
+            { name : "TaskToDo",}, 
+            { name : "Ideal", color : "#FF0000"}, 
+            { name : 'Completed', type : 'column', yAxis:1 }, 
+            { name : 'Accepted',type : 'column', yAxis:1 }
+        ];
         var hc = lumenize.arrayOfMaps_To_HighChartsSeries(calculator.getResults().seriesData, hcConfig);
 
         // display the chart
@@ -113,10 +135,13 @@ Ext.define('CustomApp', {
                 series[1],
                 series[2],
                 series[3],
-                series[4]
+                //{ name : "Ideal", data : series[3].data, color : "Black" },
+                series[4],
+                series[5]
             ]
          },
           chartConfig : {
+                colors : ["DarkGreen","LightGreen","Black"],
                 chart: {
                 },
                 title: {
@@ -163,7 +188,7 @@ Ext.define('CustomApp', {
                     }
                  },
                  line : {
-                     zIndex : 1,
+                    zIndex : 1,
                     tooltip : {
                         valueSuffix : ' hours'
                     }
